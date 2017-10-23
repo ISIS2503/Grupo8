64b281079aaa6460a1052d442a8eb243a8905965
David Narvaez <d.narvaez11@uniandes.edu.co>
2017-10-22 20:56:23 -0500

    Alerta 2

--- Experimento2/E2Utils.py
+++ Experimento2/E2Utils.py
@@ -4,36 +4,41 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+import sys
+
 smtp_client = 'smtp.office365.com'
 sender = 'e-mail'
 password = 'password'
 
 
 def sendTo(to, attachmentPath, subject, msg_body):
-    msg = MIMEMultipart()
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = " ,".join(to)
+    try:
+        msg = MIMEMultipart()
+        msg['Subject'] = subject
+        msg['From'] = sender
+        msg['To'] = " ,".join(to)
 
-    print('From:', sender, "\nTo: ", to, "\nAsunto:", subject, '\nBody:', msg_body, '\nAttachment:', attachmentPath)
+        print('From:', sender, "\nTo: ", to, "\nAsunto:", subject, '\nBody:', msg_body, '\nAttachment:', attachmentPath)
 
-    msg.attach(MIMEText(msg_body, 'plain'))
-    if attachmentPath:
-        ctype, encoding = mimetypes.guess_type(attachmentPath)
-        if ctype is None or encoding is not None:
-            ctype = "application/octet-stream"
-        maintype, subtype = ctype.split("/", 1)
+        msg.attach(MIMEText(msg_body, 'plain'))
+        if attachmentPath:
+            ctype, encoding = mimetypes.guess_type(attachmentPath)
+            if ctype is None or encoding is not None:
+                ctype = "application/octet-stream"
+            maintype, subtype = ctype.split("/", 1)
 
-        fp = open(attachmentPath, "rb")
-        attachment = MIMEBase(maintype, subtype)
-        attachment.set_payload(fp.read())
-        fp.close()
+            fp = open(attachmentPath, "rb")
+            attachment = MIMEBase(maintype, subtype)
+            attachment.set_payload(fp.read())
+            fp.close()
 
-        attachment.add_header("Content-Disposition", "attachment")
-        msg.attach(attachment)
+            attachment.add_header("Content-Disposition", "attachment")
+            msg.attach(attachment)
 
-    smtp = smtplib.SMTP(smtp_client)
-    smtp.starttls()
-    smtp.login(sender, password)
-    smtp.send_message(msg)
-    smtp.quit()
+        smtp = smtplib.SMTP(smtp_client)
+        smtp.starttls()
+        smtp.login(sender, password)
+        smtp.send_message(msg)
+        smtp.quit()
+    except:
+        print('No se pudo enviar el correo', file=sys.stderr)
--- Experimento2/SistemaDeAlertas.py
+++ Experimento2/SistemaDeAlertas.py
@@ -1,4 +1,5 @@
 import datetime
+
 import E2Utils as util
 
 # {
@@ -46,29 +47,28 @@ receivers = []
 
 
 def rules(dato):
-    add_to_queue(dato['metadata'], dato['tipo'], dato['valor'])
+    if 'valor' in dato:
+        add_to_queue(dato)
 
-    offline(dato)
-    out_of_range(dato)
-    inefficient(dato)
+        offline(dato)
+        out_of_range(dato)
+        inefficient(dato)
 
 
 def offline(dato):
     id_mc = dato['metadata']['microcontrolador']
-    tipo = dato['metadata']['tipo']
+    tipo = dato['tipo']
     area = dato['metadata']['area']
 
     queue = structure[area][(id_mc, tipo)][1]
-    if not queue:
-        queue = []
 
     timestamp = None
     now = datetime.datetime.now()
     if len(queue) == 0:
         timestamp = datetime.datetime(now.year, now.month, now.day, hora_inicio, 0, 0, 0)
     else:
-        ultimo = queue.pop(len(queue) - 1)
-        timestamp = ultimo['timestamp']
+        ultimo = queue[- 1]
+        timestamp = ultimo[1]
 
     temp = 5 * ref[tipo]['freq']
     timestamp += datetime.timedelta(seconds=temp)
@@ -79,25 +79,25 @@ def offline(dato):
 
 def out_of_range(dato):
     id_mc = dato['metadata']['microcontrolador']
-    tipo = dato['metadata']['tipo']
+    tipo = dato['tipo']
     area = dato['metadata']['area']
 
     tupla = structure[area][(id_mc, tipo)]
-    if len(tupla) >= 10:
-        suma = 0
-        for elem in tupla:
-            suma += elem['dato']
-        prom = suma / 10
+    suma = 0
+    for elem in tupla[1]:
+        suma += elem[0]
+    prom = suma / len(tupla[1])
 
-        if prom < ref[tipo]['lim_inf'] or prom > ref[tipo]['lim_sup']:
-            alerta(2, dato['metadata'])
-            tupla[0] = True
-        else:
-            tupla[0] = False
+    if prom < ref[tipo]['lim_inf'] or prom > ref[tipo]['lim_sup']:
+        alerta(2, dato)
+        tupla[0] = True
+    else:
+        tupla[0] = False
 
 
 def inefficient(dato):
-    alerta(3, dato)
+    # alerta(3, dato)
+    pass
 
 
 def alerta(tipo, data):
@@ -110,13 +110,11 @@ def alerta(tipo, data):
                    '\nMicrocontrolador: ' + metadata['microcontrolador']
         util.sendTo(receivers, None, sbj_alerta1, msg_body)
     elif tipo == 2:
-        # TODO
         # Actuador data['metadata']['area']. algo = dato
-        area_tipo = areas_alerta[(data['metadata']['area'], data['tipo'])]
-        if not area_tipo:
+        if (data['metadata']['area'], data['tipo']) not in areas_alerta:
             areas_alerta[(data['metadata']['area'], data['tipo'])] = (1, datetime.datetime.now())
-            # TODO
-            # Actuador
+            # TODO Actuador
+            print('Alerta 2', data['tipo'], data['valor'], ref[data['tipo']])
     elif tipo == 3:
         metadata = data['metadata']
         msg_body = 'El actuador en el area: ' + metadata['area'] + ', ha estado encendido por 1 hora'
@@ -124,23 +122,149 @@ def alerta(tipo, data):
 
 
 def calcular_rango(arr):
-    # arr_i = {min, max, var, freq}
-    for i in range(len(arr)):
-        ref[i]['freq'] = arr[i]['freq']
+    # arr_i = {name, min, max, var, freq}
 
-        ref[i]['lim_inf'] = arr[i]['min'] - arr[i]['var']
-        ref[i]['lim_sup'] = arr[i]['max'] - arr[i]['var']
+    for item in arr:
+        ref_i = ref[item['name']]
+
+        ref_i['freq'] = item['freq']
+        ref_i['lim_inf'] = item['min'] - item['var']
+        ref_i['lim_sup'] = item['max'] - item['var']
 
 
-def add_to_queue(metadata, tipo, value):
+def add_to_queue(dato):
+    metadata = dato['metadata']
+    tipo = dato['tipo']
+    value = dato['valor']
+    timestamp = dato['timeStamp']
+
+    if not metadata['area'] in structure:
+        structure[metadata['area']] = {}
     mcs = structure[metadata['area']]
-    datos_mc = mcs[(metadata['microcontrolador'], tipo)]
 
-    if not datos_mc:
-        datos_mc = (False, [])
+    if not (metadata['microcontrolador'], tipo) in mcs:
+        mcs[(metadata['microcontrolador'], tipo)] = [False, []]
+    datos_mc = mcs[(metadata['microcontrolador'], tipo)]
 
     if len(datos_mc[1]) == 10:
         datos_mc[1].pop(0)
-    datos_mc[1].append(value)
+    datos_mc[1].append((value, timestamp))
 
     mcs[(metadata['microcontrolador'], tipo)] = datos_mc
+    structure[metadata['area']] = mcs
+
+
+def initial_config():
+    arr = []
+    arr.append({'name': 'Temperatura', 'min': 21.5, 'max': 27.0, 'var': 5.4, 'freq': 60})
+    arr.append({'name': 'Ruido', 'min': 0, 'max': 85.0, 'var': 0, 'freq': 120})
+    arr.append({'name': 'Gases(CO)', 'min': 0, 'max': 100.0, 'var': 0, 'freq': 60})
+    arr.append({'name': 'Iluminacion', 'min': 100, 'max': 2000, 'var': 0, 'freq': 120})
+    calcular_rango(arr)
+
+
+def prueba():
+    initial_config()
+    message = {
+        "sensetime": datetime.datetime.now(),
+        "variablesAmbientales": [
+            {
+                "data": 19,
+                "tipo": "Temperatura",
+                "unidad": "C"
+            },
+            {
+                "data": 731,
+                "tipo": "Gases(CO)",
+                "unidad": "ppm"
+            },
+            {
+                "data": 65536,
+                "tipo": "Iluminacion",
+                "unidad": "Lux"
+            },
+            {
+                "data": 58,
+                "tipo": "Ruido",
+                "unidad": "dB"
+            }
+        ],
+        "metadata": {
+            "nivelId": "1",
+            "areaId": "1",
+            "microcontroladorId": "1"
+        }
+    }
+
+    sensetime = message['sensetime']
+    value = list(message['variablesAmbientales'])
+    ids = message['metadata']
+    nivel_id = ids['nivelId']
+    area_id = ids['areaId']
+    microcontrolador_id = ids['microcontroladorId']
+
+    for elem in value:
+        rules({
+            'valor': elem['data'],
+            'tipo': elem['tipo'],
+            'timeStamp': sensetime,
+            'metadata': {
+                'nivel': nivel_id,
+                'area': area_id,
+                'microcontrolador': microcontrolador_id,
+            }
+        })
+
+    message = {
+        "sensetime": datetime.datetime.now() + datetime.timedelta(hours=100),
+        "variablesAmbientales": [
+            {
+                "data": 19,
+                "tipo": "Temperatura",
+                "unidad": "C"
+            },
+            {
+                "data": 731,
+                "tipo": "Gases(CO)",
+                "unidad": "ppm"
+            },
+            {
+                "data": 65536,
+                "tipo": "Iluminacion",
+                "unidad": "Lux"
+            },
+            {
+                "data": 58,
+                "tipo": "Ruido",
+                "unidad": "dB"
+            }
+        ],
+        "metadata": {
+            "nivelId": "1",
+            "areaId": "1",
+            "microcontroladorId": "1"
+        }
+    }
+
+    sensetime = message['sensetime']
+    value = list(message['variablesAmbientales'])
+    ids = message['metadata']
+    nivel_id = ids['nivelId']
+    area_id = ids['areaId']
+    microcontrolador_id = ids['microcontroladorId']
+
+    for elem in value:
+        rules({
+            'valor': elem['data'],
+            'tipo': elem['tipo'],
+            'timeStamp': sensetime,
+            'metadata': {
+                'nivel': nivel_id,
+                'area': area_id,
+                'microcontrolador': microcontrolador_id,
+            }
+        })
+
+
+prueba()
+print(structure)
